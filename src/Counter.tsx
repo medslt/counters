3b285641d95b543cbbc01ca7c2472719e6dec3f2
mohamed salamat <moahmed.salamat@gmail.com>
2023-02-25 17:35:32 +0000

    formatting

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,21 +1,32 @@
-import {memo} from 'react';
-import {CounterType} from './types'
-
+import { memo } from "react"
+import { CounterType } from "./types"
 
 type CounerPropsType = {
-    counter: CounterType,
-    increment: (id: number) => void,
-    decrement: (id: number) => void
-  }
-  
-const Counter = memo(({counter, increment, decrement}: CounerPropsType)  => (
-      <div data-testid="counter-element">
-        <b>{counter.value}</b>
-        <div>
-          <button onClick={() => {increment(counter.id)}}>+</button>
-          <button onClick={() => {decrement(counter.id)}}>-</button>
-        </div>
-      </div>
-  ))
+  counter: CounterType
+  increment: (id: number) => void
+  decrement: (id: number) => void
+}
+
+const Counter = memo(({ counter, increment, decrement }: CounerPropsType) => (
+  <div data-testid="counter-element">
+    <b>{counter.value}</b>
+    <div>
+      <button
+        onClick={() => {
+          increment(counter.id)
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          decrement(counter.id)
+        }}
+      >
+        -
+      </button>
+    </div>
+  </div>
+))
 
 export default Counter
